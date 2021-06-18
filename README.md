5fa74ab2960bb0277b495fe5bb609f335014d1bf
Joe Spandrusyszyn <github@illandril.net>
2021-06-17 20:35:33 -0400

    Initial release

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# FoundryVTT-sheet-lockdown-worldbuilding
+# Illandril's Character Sheet Lockdown (Simple World-Building)
+![Latest Release Download Count](https://img.shields.io/github/downloads/illandril/FoundryVTT-sheet-lockdown-worldbuilding/latest/module.zip?color=4b0000&label=Downloads)
+![Forge Installs](https://img.shields.io/badge/dynamic/json?color=4b0000&label=Forge%20Installs&query=package.installs&url=http%3A%2F%2Fforge-vtt.com%2Fapi%2Fbazaar%2Fpackage%2Fillandril-sheet-lockdown-worldbuilding&suffix=%25)
+![Foundry Core Compatible Version](https://img.shields.io/badge/dynamic/json?color=4b0000&label=Foundry%20Version&query=$.compatibleCoreVersion&url=https%3A%2F%2Fgithub.com%2Fillandril%2FFoundryVTT-sheet-lockdown-worldbuilding%2Freleases%2Flatest%2Fdownload%2Fmodule.json)
+
+This is a module for the the Simple World-building System for Foundry Virtual Tabletop that prevents players from making changes to their character sheets.
+
+Note that this is not a secure lockdown and players could get around the restrictions, so this is not a sufficient answer if your players are malicious. This is intended for games where the players are inexperienced (with your game and/or with using computers) to prevent them from making mistakes (ex. accidentally adding/removing an attribute).
+
+If you have any requests (including support for a specific custom character sheet), please write an issue here with details on what you are looking for.
+
+# Character Sheet Lockdown for other Systems
+* [Character Sheet Lockdown for D&D 5e](https://github.com/illandril/FoundryVTT-sheet5e-lockdown)
+If you would like this behavior for other Systems, feel free to write an issue and I will consider it. My time is limited, and character sheets for systems vary considerably, so do not be surprised if I either take a long time to create a module for your preferred system, or don't create one at all. If you know of any other modules that already provide similar behavior for other systems, please let me know so I can link to them here for others who might be interested in that behavior.
---
+++ module/scripts/index.js
@@ -0,0 +1,77 @@
+import Settings from './settings.js';
+import { CSS_PREFIX } from './module.js';
+
+const CSS_SHEET = `${CSS_PREFIX}sheet`;
+const CSS_LOCK = `${CSS_PREFIX}lock`;
+const CSS_EDIT = `${CSS_PREFIX}edit`;
+const CSS_TOGGLE_EDIT_ON = `${CSS_PREFIX}toggleEditOn`;
+const CSS_TOGGLE_EDIT_OFF = `${CSS_PREFIX}toggleEditOff`;
+const CSS_HIDE = `${CSS_PREFIX}hide`;
+const CSS_NO_POINTER_EVENTS = `${CSS_PREFIX}noPointerEvents`;
+
+Hooks.on('renderActorSheet', (actorSheet) => {
+  if (!actorSheet.isEditable) {
+    return;
+  }
+  const sheetElem = actorSheet.element[0];
+  if (!sheetElem) {
+    return;
+  }
+
+  if (!sheetElem.classList.contains(CSS_SHEET)) {
+    sheetElem.classList.add(CSS_SHEET);
+    sheetElem.classList.add(CSS_LOCK);
+    if (game.user.hasRole(Settings.ShowToggleEditRole.get())) {
+      const sheetHeader = sheetElem.querySelector('.window-header');
+      const sheetTitle = sheetHeader.querySelector('.window-title');
+
+      const editOnLink = document.createElement('a');
+      editOnLink.appendChild(faIcon('lock'));
+      editOnLink.classList.add(CSS_TOGGLE_EDIT_ON);
+      const toggleOnString = game.i18n.localize('illandril-sheet-lockdown-worldbuilding.toggleEditOn');
+      editOnLink.appendChild(document.createTextNode(toggleOnString));
+      editOnLink.addEventListener('click', () => makeLocked(sheetElem, false), false);
+      editOnLink.addEventListener('dblclick', stopPropagation, false);
+      sheetHeader.insertBefore(editOnLink, sheetTitle.nextSibling);
+
+      const editOffLink = document.createElement('a');
+      editOffLink.appendChild(faIcon('unlock'));
+      editOffLink.classList.add(CSS_TOGGLE_EDIT_OFF);
+      const toggleOffString = game.i18n.localize('illandril-sheet-lockdown-worldbuilding.toggleEditOff');
+      editOffLink.appendChild(document.createTextNode(toggleOffString));
+      editOffLink.addEventListener('click', () => makeLocked(sheetElem, true), false);
+      editOffLink.addEventListener('dblclick', stopPropagation, false);
+      sheetHeader.insertBefore(editOffLink, sheetTitle.nextSibling);
+    }
+    makeLocked(sheetElem, sheetElem.classList.contains(CSS_LOCK));
+  }
+});
+
+const makeLocked = (sheetElem, locked) => {
+  addRemoveClass(sheetElem, CSS_LOCK, locked);
+  addRemoveClass(sheetElem, CSS_EDIT, !locked);
+  for (let elem of sheetElem.querySelectorAll('[data-action],.group-controls,.editor-edit')) {
+    addRemoveClass(elem, CSS_HIDE, locked);
+  }
+  for (let elem of sheetElem.querySelectorAll('input,select')) {
+    elem.disabled = locked;
+  }
+  for (let elem of sheetElem.querySelectorAll('.profile-img')) {
+    addRemoveClass(elem, CSS_NO_POINTER_EVENTS, locked);
+  }
+};
+
+const addRemoveClass = (element, cssClass, isAdd) => {
+  isAdd ? element.classList.add(cssClass) : element.classList.remove(cssClass);
+};
+
+const faIcon = (name) => {
+  const icon = document.createElement('i');
+  icon.classList.add('fas');
+  icon.classList.add('fa-' + name);
+  return icon;
+};
+
+const stopPropagation = (event) => {
+  event.stopPropagation();
+};
